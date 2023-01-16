1d993a4d28ddd8fdb92280d43735333184307eb9
Pavel Zwerschke <pavelzw@gmail.com>
2023-01-16 23:45:07 +0100

    Cleanup

--- Formula/micromamba.rb
+++ Formula/micromamba.rb
@@ -1,35 +1,21 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Micromamba < Formula
-  desc ""
-  homepage ""
-  license ""
-  head "https://github.com/mamba-org/mamba.git", branch: "main"
-  # url "https://github.com/mamba-org/mamba/archive/refs/tags/2023.01.16.tar.gz"
-  # url "https://github.com/mamba-org/mamba.git", :using => :git, :head => true
+  desc "The Fast Cross-Platform Package Manager"
+  homepage "https://github.com/mamba-org/mamba"
   url "https://github.com/mamba-org/mamba/archive/refs/tags/2023.01.16.tar.gz"
-  # version "1.2"
+  sha256 "TODO"
+  license "BSC-3-Clause"
+  version "1.2.0"
 
-  # dependencies as in 
-  # https://github.com/mamba-org/mamba/blob/3390da9ab93424fd40d7584ea869b6ef27e212f8/micromamba/recipe/meta.yaml#L11
-  
-  # build dependencies
-  # needs c/cxx compiler -> already installed?
   depends_on "cmake" => :build
-  depends_on "ninja" => :build
 
-  # marked as 'win'(dows)
-  depends_on "vcpkg" => :build
-  depends_on "python" => :build
-  uses_from_macos "curl" # depends_on "curlpp" => :build # c++ wrapper for libCURL
+  uses_from_macos "curl"
+  # depends_on "curlpp" => :build TODO?
   uses_from_macos "zlib"
 
-  # host dependencies
-  depends_on "cli11" # TODO: specify version
-  depends_on "tl-expected" # TODO: add formula as well
+  depends_on "cli11"
+  depends_on "tl-expected"
   depends_on "nlohmann-json"
-  depends_on "spdlog" # TODO: specify version
+  depends_on "spdlog"
   depends_on "fmt"
   depends_on "yaml-cpp"
   depends_on "xz"
@@ -41,38 +27,26 @@ class Micromamba < Formula
   depends_on "zstd"
   depends_on "lz4"
   depends_on "reproc"
-  # depends_on "reproc-cpp" # not available 
 
   def install
-
-    # install vcpkg and tl-expected
-    # system "git clone https://github.com/Microsoft/vcpkg.git"
-    # system "./vcpkg/bootstrap-vcpkg.sh"
-    # ENV["VCPKG_ROOT"] = './vcpkg'
-    # system "vcpkg install tl-expected"
-
-    build_args = %W[
+    args = %w[
       -DBUILD_LIBMAMBA=ON
       -DBUILD_SHARED=ON
       -DBUILD_MICROMAMBA=ON
       -DMICROMAMBA_LINKAGE=DYNAMIC
     ]
-    # -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
 
     mkdir "build" do
-      system "cmake", *(std_cmake_args + build_args), ".."
+      system "cmake", "..", *args, *std_cmake_args
       system "make"
       system "make", "install"
     end
+  end
 
-    # move final executable to the correct location
-    # lib.install "build/libmamba/libmamba.dylib"
-    # bin.install "build/micromamba/micromamba"
+  test do
+    assert_match version, shell_output("#{bin}/micromamba --version").strip
 
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    # system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "{bin}/micromamba", "create", "-n", "test", "python=3.9", "-y"
+    system "{bin}/micromamba", "run", "-n", "test", "python", "-c", "import sys; sys.exit(0)"
   end
 end
--- Formula/tl-expected.rb
+++ Formula/tl-expected.rb
@@ -2,20 +2,19 @@ class TlExpected < Formula
   desc "C++11/14/17 std::expected with functional-style extensions"
   homepage "https://github.com/TartanLlama/expected"
   url "https://github.com/TartanLlama/expected/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 ""
+  sha256 "TODO"
   license "TODO"
 
   depends_on "cmake" => :build
 
   def install
-
-    build_args = %W[
+    args = %w[
       -DEXPECTED_ENABLE_TESTS=OFF
       -DCMAKE_INSTALL_LIBDIR=#{lib}
     ]
 
     mkdir "build" do
-      system "cmake", "..", *(std_cmake_args + build_args)
+      system "cmake", "..", *args, *std_cmake_args
       system "make", "install"
     end
   end
