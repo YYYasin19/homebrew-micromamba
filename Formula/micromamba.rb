b68fc0234910adb115f8eb4954ed2dbb556e468c
Pavel Zwerschke <pavelzw@gmail.com>
2023-01-16 23:45:29 +0100

    Hotfix 🚒

--- Formula/micromamba.rb
+++ Formula/micromamba.rb
@@ -46,7 +46,7 @@ class Micromamba < Formula
   test do
     assert_match version, shell_output("#{bin}/micromamba --version").strip
 
-    system "{bin}/micromamba", "create", "-n", "test", "python=3.9", "-y"
-    system "{bin}/micromamba", "run", "-n", "test", "python", "-c", "import sys; sys.exit(0)"
+    system "#{bin}/micromamba", "create", "-n", "test", "python=3.9", "-y"
+    system "#{bin}/micromamba", "run", "-n", "test", "python", "-c", "import sys; sys.exit(0)"
   end
 end
